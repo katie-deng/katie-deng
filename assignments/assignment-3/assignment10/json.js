e05bbe0d1b7a090d44f55f5703245c94bb681de5
katie-deng <kade6270@colorado.edu>
2022-10-27 21:45:06 -0600

    102722 - assignment10

---
+++ assignments/assignment-3/assignment10/json.js
@@ -0,0 +1,99 @@
+// Question 1: employee details -------------------------------------
+var resources = {
+    "employees" : [
+        {
+            "name" : "Sam",
+            "department" : "Tech",
+            "designation" : "Manager",
+            "salary" : 40000,
+            "raise" : true,
+        },
+        {
+            "name" : "Mary",
+            "department" : "Finance",
+            "designation" : "Trainee",
+            "salary" : 18500,
+            "raise" : true,
+        },
+        {
+            "name" : "Bill",
+            "department" : "HR",
+            "designation" : "Executive",
+            "salary" : 21200,
+            "raise" : false,
+        }
+    ]
+}
+
+console.log (resources.employees)
+console.log (resources.employees[2].name)
+
+// Question 2: company details -------------------------------------
+var resources = {
+    "companyName" : "Tech Stars",
+    "website" : "www.techstars.site",
+    "employees" : [
+        {
+            "name" : "Sam",
+            "department" : "Tech",
+            "designation" : "Manager",
+            "salary" : 40000,
+            "raise" : true,
+        },
+        {
+            "name" : "Mary",
+            "department" : "Finance",
+            "designation" : "Trainee",
+            "salary" : 18500,
+            "raise" : true,
+        },
+        {
+            "name" : "Bill",
+            "department" : "HR",
+            "designation" : "Executive",
+            "salary" : 21200,
+            "raise" : false,
+        },
+    ]
+}
+
+console.log(resources.companyName)
+console.log(resources.website)
+
+// Question 3: new employee -------------------------------------
+
+let newEmployee = 
+    {
+        "name" : "Anna",
+        "department" : "Tech",
+        "designation" : "Executive",
+        "salary" : 25600,
+        "raise" : false,
+    }
+
+resources["employees"].push (newEmployee)
+
+console.log(resources.employees.length)
+console.log(resources.employees[3].name)
+
+// Question 4: total salary -------------------------------------
+
+let total = 0
+for (let i = 0; i < resources.employees.length; i++) {
+    total += resources.employees[i].salary
+}
+console.log(total)
+
+// Question 5: raises -------------------------------------
+
+function raiseSalary (salary) {
+    for (let j = 0; j < resources.employees.length; j++) {
+        if (resources.employees[j].raise === true) {
+            let increaseSalary = resources.employees[j].salary * 1.1
+            resources.employees[j].salary = increaseSalary
+            // resources.employees[j].raise === true
+        }  
+    }
+}
+
+console.log(resources.employees)
